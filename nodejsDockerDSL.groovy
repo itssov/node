fb718231f6c628388161fa8af83b2e5908d9d767
itssov <103154182+itssov@users.noreply.github.com>
2022-04-28 14:19:00 -0700

    Create nodejsDockerDSL.groovy

---
+++ nodejsDockerDSL.groovy
@@ -0,0 +1,44 @@
+job('Frank Node.js Docker DSL') {
+    description('Practica de Francisco Valenzuela Node JS Docker DSL para el curso de Jenkins')
+    scm {
+        git('https://github.com/itssov/nodejsapp.git', 'master') { node ->
+            node / gitConfigName('itssov')
+            node / gitConfigEmail('PDS1fv@gmail.com')
+        }
+    }
+    triggers {
+        scm('H/7 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs')
+    }
+    steps {
+        dockerBuildAndPublish {
+            repositoryName('itssov/nodejsapp')
+            tag('${GIT_REVISION,length=7}')
+            registryCredentials('docker-hub')
+            forcePull(false)
+            createFingerprints(false)
+            skipDecorate()
+        }
+    }
+    publishers {
+	slackNotifier {
+            notifyAborted(true)
+            notifyEveryFailure(true)
+            notifyNotBuilt(false)
+            notifyUnstable(false)
+            notifyBackToNormal(true)
+            notifySuccess(true)
+            notifyRepeatedFailure(false)
+            startNotification(false)
+            includeTestSummary(false)
+            includeCustomMessage(false)
+            customMessage(null)
+            sendAs(null)
+            commitInfoChoice('NONE')
+            teamDomain(null)
+            authToken(null)
+        }
+    }
+}
